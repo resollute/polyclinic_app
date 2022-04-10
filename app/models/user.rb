b0db3f89506b1ee84873bce9e37c808d69e61b3b
resollute <vteleleiko@gmail.com>
2022-04-10 20:58:45 +0300

    some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validates :phone, uniqueness: true, presence: { message: "must be numerical" },
+  #                                     numericality: true,
+  #                                     length: { minimum: 10, maximum: 15 }
   validates :phone, uniqueness: true, presence: { message: "must be numerical" },
                                       numericality: true,
-                                      length: { minimum: 10, maximum: 15 }
+                                      format: { with: /^\+?3?8?(0(67|68|96|97|98)\d{7})$/, multiline: true }
 
   def email_required?
     false
@@ -15,5 +18,8 @@ class User < ApplicationRecord
 
   def email_changed?
     false
+  end
+
+  def will_save_change_to_email?
   end
 end
